e54b5767f4db833d1b0e41ee949b7d4ca1eade05
Chris Johnson <chrisjohnson00@hotmail.com>
2023-03-23 14:25:54 -0700

    chore: adding probe endpoints (#2)
    * chore: adding probe endpoints
    
    * ci: rc based container tags, not branch names

--- webapp_sample/application.py
+++ webapp_sample/application.py
@@ -1,7 +1,7 @@
 from services.backend import Backend
 from utilities.env_config import get_config
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, jsonify
 )
 
 bp = Blueprint('app', __name__, url_prefix='/')
@@ -17,3 +17,13 @@ def users():
     backend_service = Backend(api_hostname=get_config('API_HOSTNAME'))
     users = backend_service.get_users()
     return render_template('app/users.html', users=users)
+
+
+@bp.route('/ready')
+def health():
+    return jsonify(success=True)
+
+
+@bp.route('/liveness')
+def liveness():
+    return jsonify(success=True)
